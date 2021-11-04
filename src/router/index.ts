57bce2aad0446ba1dda0f432c4871c3edeb00231
Irvin López <irvin_21_95@hotmail.com>
2021-11-04 08:29:22 -0600

    login view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import Layout from "@/views/Layout.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  { path: "", component: Layout },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Register.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="text-center">
+    <main class="form-signin">
+      <form @submit.prevent="submit">
+        <img
+          class="mb-4"
+          src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="first_name"
+            placeholder="First Name"
+            v-model="first_name"
+          />
+          <label for="first_name">First Name</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="last_name"
+            placeholder="Last Name"
+            v-model="last_name"
+          />
+          <label for="last_name">Last Name</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+            v-model="email"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            v-model="password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="ConfirmPassword"
+            placeholder="Password Confirm"
+            v-model="password_confirm"
+          />
+          <label for="ConfirmPassword">Password Confirm</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </main>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      password_confirm: "",
+    };
+  },
+  methods: {
+    async submit() {
+      await axios.post("http://localhost:8000/api/admin/register", {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.password_confirm,
+      });
+
+      await this.$router.push("/login");
+    }
+  }
+};
+</script>
+
+<style>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
